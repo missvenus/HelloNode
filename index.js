e627caa6dd1a61a62b6ece4e774ff685ea197639
Venus Jain - WW <jainvw1@WGB01ML106614.local>
2015-09-27 15:43:37 +0100

    Initisal push

---
+++ index.js
@@ -0,0 +1,35 @@
+// This is the main file of our chat app. It initializes a new 
+// express.js instance, requires the config and routes files
+// and listens on a port. Start the application by running
+// 'node app.js' in your terminal
+
+
+var express = require('express'),
+	app = express();
+
+// This is needed if the app is run on heroku:
+
+var port = process.env.PORT || 8081;
+
+ // Set .html as the default template extension
+	app.set('view engine', 'html');
+
+	// Initialize the ejs template engine
+	app.engine('html', require('ejs').renderFile);
+
+	// Tell express where it can find the templates
+	app.set('views', __dirname + '/views');
+
+	// Make the files in the public folder available to the world
+	app.use(express.static(__dirname + '/public'));
+	
+
+	app.get('/', function(req, res){
+
+		// Render views/home.html
+		res.render('home');
+	});
+
+app.listen(port);
+
+console.log('Your application is running on http://localhost:' + port);
